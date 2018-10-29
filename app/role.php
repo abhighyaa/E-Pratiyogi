69140cef7a9685b54b2f3a21c29031f32b00a30b
Abhighyaa <abhighyaajain1610@gmail.com>
2018-10-29 21:28:12 +0530

    teacher panel

--- app/role.php
+++ app/role.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class role extends Model
 {
-<<<<<<< HEAD
-    //
-=======
     public function users()
     {
         return $this->belongsToMany(User::class,'role_users');
     }
->>>>>>> a33104e9578b1c5fbb66d21bf43b8abae165471a
 }
